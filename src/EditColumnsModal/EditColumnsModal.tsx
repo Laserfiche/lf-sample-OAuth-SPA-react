c355fbd5777f650aeef1407b5b16cdae06788f2f
rfulton-lf <robert.fulton@laserfiche.com>
2023-05-10 13:05:55 -0700

    made EditColumnsModal into a functional component

--- src/EditColumnsModal/EditColumnsModal.tsx
+++ src/EditColumnsModal/EditColumnsModal.tsx
@@ -1,5 +1,5 @@
 import { LfLocalizationService } from "@laserfiche/lf-js-utils";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ColumnDef } from "@laserfiche/types-lf-ui-components";
 import "./EditColumnsModal.css";
 
@@ -22,119 +22,113 @@ const resources: Map<string, object> = new Map<string, object>([
   ],
 ]);
 
-export default class EditColumnsModal extends React.Component<
-  {
-    onClose: (columns?: ColumnDef[]) => void;
-    initialColumnsSelected: ColumnDef[];
-    allColumnOptions: ColumnDef[];
-  },
-  { columnsSelected: ColumnDef[]; open: boolean }
-> {
-  localizationService: LfLocalizationService = new LfLocalizationService(
-    resources
-  );
-  closeOnEscapeKeyDown = (e: KeyboardEvent) => {
-    if (e.code === "Escape") {
-      this.props.onClose();
+const localizationService: LfLocalizationService = new LfLocalizationService(
+  resources
+);
+
+const OK = localizationService.getString("OK");
+const CANCEL = localizationService.getString("CANCEL");
+const ADD_REMOVE_COLUMNS: string =
+  localizationService.getString("ADD_REMOVE_COLUMNS");
+
+export default function EditColumnsModal(props: {
+  onClose: (columns?: ColumnDef[]) => void;
+  initialColumnsSelected: ColumnDef[];
+  allColumnOptions: ColumnDef[];
+}) {
+  const [open, setOpen] = useState<boolean>(false);
+  const [columnsSelected, setColumnsSelected] = useState<ColumnDef[]>(props.initialColumnsSelected);
+
+
+  const closeOnEscapeKeyDown = (keyboardEvent: KeyboardEvent) =>  {
+    if (keyboardEvent.code === "Escape") {
+      props.onClose();
     }
   };
-
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      columnsSelected: this.props.initialColumnsSelected,
-      open: false,
-    };
-  }
-
-  componentDidMount() {
-    document.body.addEventListener("keydown", this.closeOnEscapeKeyDown);
+  useEffect(() => {
+    document.body.addEventListener("keydown", closeOnEscapeKeyDown);
     setTimeout(() => {
-      this.setState({ open: true });
+      setOpen(true);
     });
-  }
 
-  closeDialog = (columns?: ColumnDef[]) => {
-    this.props.onClose(columns);
+  });
+
+  const closeDialog = (columns?: ColumnDef[]) => {
+    props.onClose(columns);
   };
 
-  onCheckboxChange(column: ColumnDef) {
+  function onCheckboxChange(column: ColumnDef) {
     // we provide a new array (not just new values) to trigger change detection
     let newSelected: ColumnDef[];
-    if (this.state.columnsSelected.includes(column)) {
-      newSelected = [...this.state.columnsSelected.filter((c) => c != column)];
+    if (columnsSelected.includes(column)) {
+      newSelected = [...columnsSelected.filter((c) => c != column)];
     } else {
-      newSelected = [...this.state.columnsSelected];
+      newSelected = [...columnsSelected];
       newSelected.push(column);
     }
-    this.setState({ columnsSelected: newSelected });
+    setColumnsSelected(newSelected);
   }
 
-  render() {
-    return (
-      <div className="edit-columns-dialog-modal">
-        <div
-          className={`edit-columns-dialog-modal-content ${
-            this.state?.open ? "show" : ""
-          }`}
-          id="edit-columns-dialog-content"
-          onClick={(e) => e.stopPropagation()}
-        >
-          <div className="edit-columns-dialog-modal-title">
-            <span className="lf-dialog-title lf-popup-dialog-title">
-              {this.ADD_REMOVE_COLUMNS}
+  return (
+    <div className="edit-columns-dialog-modal">
+      <div
+        className={`edit-columns-dialog-modal-content ${
+          open ? "show" : ""
+        }`}
+        id="edit-columns-dialog-content"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <div className="edit-columns-dialog-modal-title">
+          <span className="lf-dialog-title lf-popup-dialog-title">
+            {ADD_REMOVE_COLUMNS}
+          </span>
+          <button
+            className="lf-close-buttton"
+            id="edit-columns-dialog-modal-close-dialog"
+            onClick={() => closeDialog()}
+          >
+            <span
+              id="edit-columns-dialog-close-icon"
+              className="material-icons"
+            >
+              close
             </span>
-            <button
-              className="lf-close-buttton"
-              id="edit-columns-dialog-modal-close-dialog"
-              onClick={() => this.closeDialog()}
-            >
-              <span
-                id="edit-columns-dialog-close-icon"
-                className="material-icons"
-              >
-                close
-              </span>
-            </button>
-          </div>
-          <div className="lf-dialog-message">
-            <div className="column-list">
-              {this.props.allColumnOptions.map((column: ColumnDef) => (
-                <div key={column.id}>
-                  <input
-                    type="checkbox"
-                    checked={this.state.columnsSelected.includes(column)}
-                    id={`${column.id}-checkbox`}
-                    onChange={() => this.onCheckboxChange(column)}
-                  ></input>
-                  <label htmlFor={`${column.id}-checkbox`}>
-                    {" "}
-                    {column.displayName}
-                  </label>
-                </div>
-              ))}
-            </div>
-          </div>
-          <div className="lf-dialog-actions">
-            <button
-              onClick={() => this.closeDialog(this.state.columnsSelected)}
-              className="lf-button primary-button"
-            >
-              {this.OK}
-            </button>
-            <button
-              onClick={() => this.closeDialog()}
-              className="lf-button sec-button margin-left-button"
-            >
-              {this.CANCEL}
-            </button>
+          </button>
+        </div>
+        <div className="lf-dialog-message">
+          <div className="column-list">
+            {props.allColumnOptions.map((column: ColumnDef) => (
+              <div key={column.id}>
+                <input
+                  type="checkbox"
+                  checked={columnsSelected.includes(column)}
+                  id={`${column.id}-checkbox`}
+                  onChange={() => onCheckboxChange(column)}
+                ></input>
+                <label htmlFor={`${column.id}-checkbox`}>
+                  {" "}
+                  {column.displayName}
+                </label>
+              </div>
+            ))}
           </div>
         </div>
+        <div className="lf-dialog-actions">
+          <button
+            onClick={() => closeDialog(columnsSelected)}
+            className="lf-button primary-button"
+          >
+            {OK}
+          </button>
+          <button
+            onClick={() => closeDialog()}
+            className="lf-button sec-button margin-left-button"
+          >
+            {CANCEL}
+          </button>
+        </div>
       </div>
-    );
-  }
-  OK = this.localizationService.getString("OK");
-  CANCEL = this.localizationService.getString("CANCEL");
-  ADD_REMOVE_COLUMNS: string =
-    this.localizationService.getString("ADD_REMOVE_COLUMNS");
+    </div>
+  );
 }
+
