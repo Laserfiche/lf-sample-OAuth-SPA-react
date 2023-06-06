2d0c6b8982c10b5bd65bb72a024f6058e36f5de3
rfulton-lf <robert.fulton@laserfiche.com>
2023-06-06 14:46:07 -0700

    moved config.ts to root

---
+++ config.ts
@@ -0,0 +1,8 @@
+const config = {
+  REDIRECT_URI: 'http://localhost:3000', // i.e http://localhost:3000, https://serverName/lf-sample/index.html
+  CLIENT_ID: '6bd54321-2737-4a42-985d-abac41375af5',
+  HOST_NAME: 'a.clouddev.laserfiche.com', // only update this if you are using a different environment (e.g. a.clouddev.laserfiche.com)
+  SCOPE: 'repository.Read repository.Write', // Scope(s) requested by the app
+};
+
+export default config;
--- src/config.ts
+++
@@ -1,8 +0,0 @@
-const config = {
-  REDIRECT_URI: 'REPLACE_WITH_YOUR_REDIRECT_URI', // i.e http://localhost:3000, https://serverName/lf-sample/index.html
-  CLIENT_ID: 'REPLACE_WITH_YOUR_CLIENT_ID',
-  SCOPE: 'repository.Read repository.Write', // Scope(s) requested by the app
-  HOST_NAME: 'laserfiche.com', // only add this if you are using a different environment (e.g. a.clouddev.laserfiche.com)
-};
-
-export default config;
