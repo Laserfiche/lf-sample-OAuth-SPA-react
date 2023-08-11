b7731e35fb50775dedc13b574bcf19914738c552
anirudh-satish <141184676+anirudh-satish@users.noreply.github.com>
2023-08-10 17:30:06 -0700

    config changed

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 const config = {
-  REDIRECT_URI: 'REPLACE_WITH_YOUR_REDIRECT_URI', // i.e http://localhost:3000, https://serverName/lf-sample/index.html
-  CLIENT_ID: 'REPLACE_WITH_YOUR_CLIENT_ID',
+  REDIRECT_URI: 'http://localhost/lf-sample-app-react', // i.e http://localhost:3000, https://serverName/lf-sample/index.html
+  CLIENT_ID: 'afd96c0c-fe6a-40bc-8992-565ff6af3984',
   HOST_NAME: 'laserfiche.com', // only update this if you are using a different environment (e.g. a.clouddev.laserfiche.com)
   SCOPE: 'repository.Read repository.Write', // Scope(s) requested by the app
 };
